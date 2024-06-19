7ee281db863152231768c0c08b4297d2fe44a584
erkinshox <shaymurodoverkinshoh@gmail.com>
2024-06-19 15:12:43 +0500

    adfjsadklf

--- js/main.js
+++ js/main.js
@@ -7,3 +7,16 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+// navbar  shirink
+window.addEventListener("scroll", function () {
+  shrink();
+});
+let navbar = document.getElementById("navbar");
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
